4d4c6b0d78cf13cb9817ed2d2ba46dcae09393bf
yavuzaksoy <yavuz@milgamestudio.com>
2023-07-25 00:36:17 +0300

    Bullet class constructed

---
+++ Assets/Scenes/BulletBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public int tier;
+    public string buuletName;
+    public float power;
+    public GameObject bulletModel;
+}
